7462b57377e85f2bac43d00ebb679e67132bf812
unknown <kanchanchavan2511@gmail.com>
2023-01-13 23:34:59 +0530

    counter added

--- static/js/index.js
+++ static/js/index.js
@@ -7,3 +7,16 @@ $(document).ready(function () {
 
 });
 
+$(document).ready(function () {
+    $('.about-count').each(function () {
+        $(this).prop('Counter', 0).animate({
+            Counter: $(this).text()
+        }, {
+            duration: 4000,
+            easing: 'swing',
+            step: function (now) {
+                $(this).text(Math.ceil(now));
+            }
+        });
+    });
+});
